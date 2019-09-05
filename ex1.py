5a59137b587ddd0f8e9f7374d53cd4d96435375e
Maxim Ruzmaykin <mruzmaykin@gmail.com>
2019-09-05 14:12:32 -0700

    adds screenshot

--- ex1.py
+++ ex1.py
@@ -4,14 +4,23 @@ from scipy.special import factorial
 arr1d = np.arange(12)
 arr2d = [[1,2,3],[6,5,4]]
 
-print(arr1d)
-print(arr2d)
 
+print("=================================")
+print("Part 1.")
+print("Arr1d:\n",arr1d)
+print("Arr2d:\n",arr2d)
 s = arr1d[4:7]
 s[1] = 0
 
-print(arr1d)
-print(s)
+print("=================================")
+print("Part 2.")
+print("Arr1d:\n",arr1d)
+print("Slice s:\n",s)
+
+print("=================================")
 
 arr2d = factorial(arr2d,exact=True )
-print(arr2d)
+print("Part 3.")
+print("Factorialized Arr2d:\n",arr2d)
+
+print("=================================")
